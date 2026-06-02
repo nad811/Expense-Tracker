9d92e07e0a9871e48dd17fb1824825f6cf00f862
nad811 <nad349393@gmail.com>
2026-06-02 14:13:15 +0530

    Create menu.py

---
+++ menu.py
@@ -0,0 +1,13 @@
+def display_menu():
+
+    print("\nExpense Tracker")
+
+    print("1. Add Expense")
+
+    print("2. View Expenses")
+
+    print("3. Delete Expense")
+
+    print("4. Summary")
+
+    print("5. Exit")
